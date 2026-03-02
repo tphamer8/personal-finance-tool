fddadf62db65b4d5bc55fb193c0b9c7dc2101641
tphamer8 <tm.phamer@gmail.com>
2026-03-01 23:02:33 -0500

    Added statement date parsing to the fidelity_parser

--- src/fidelity_parser.py
+++ src/fidelity_parser.py
@@ -1,6 +1,9 @@
 import csv
 from dataclasses import dataclass
 from typing import Tuple, List
+import os
+import re
+from datetime import date
 
 @dataclass
 class StatementHeader:
@@ -9,6 +12,7 @@ class StatementHeader:
     beginning_value: float
     ending_value: float
     dividends: float
+    statement_date: date
 
 @dataclass
 class Holding:
@@ -20,6 +24,27 @@ class Holding:
     beginning_value: float
     ending_value: float
     cost_basis: float
+
+def parse_date_from_filename(file_path: str) -> date:
+    """ Parses date from filename e.g. Statement1312026.csv → 2026-01-31 """
+    filename = os.path.basename(file_path)
+    filename = os.path.splitext(filename)[0] 
+
+    # extract digits after "Statement"
+    digits = re.search(r'Statement(\d+)', filename).group(1)
+
+    # extract month, day, & year, check if month is 1 or 2 digits
+    if len(digits) == 7: # e.g. "1312026" → M DD YYYY
+        month = int(digits[0:1])
+        day   = int(digits[1:3])
+        year  = int(digits[3:])
+    elif len(digits) == 8: # e.g. "10122026" → MM DD YYYY
+        month = int(digits[0:2])
+        day   = int(digits[2:4])
+        year  = int(digits[4:])
+
+    return date(year, month, day)
+
 
 def parse_fidelity_statement(file_path: str) -> tuple[StatementHeader, List[Holding]]:
     """ Parses a CSV statement and returns a list of transactions """
@@ -38,7 +63,8 @@ def parse_fidelity_statement(file_path: str) -> tuple[StatementHeader, List[Hold
             account_type    = statement_header[0],
             beginning_value = float(statement_header[2]),
             ending_value    = float(statement_header[4]),
-            dividends       = float(statement_header[7]) if statement_header[7] else 0.0
+            dividends       = float(statement_header[7]) if statement_header[7] else 0.0,
+            statement_date  = parse_date_from_filename(file_path)
         )
 
         # Read row by row
@@ -82,8 +108,10 @@ def parse_fidelity_statement(file_path: str) -> tuple[StatementHeader, List[Hold
 if __name__ == "__main__":
 
     indiv_1_31_2026 = "data/raw/fidelity/Statement1312026.csv"
-    header1, holdings1 = parse_fidelity_statement(indiv_1_31_2026)
+    indiv_12_31_2025 = "data/raw/fidelity/Statement12312025.csv"
+    # header1, holdings1 = parse_fidelity_statement(indiv_1_31_2026)
+    header2, holdings2 = parse_fidelity_statement(indiv_12_31_2025)
 
-    print(f"Header: {header1}")
-    for holding in holdings1:
+    print(f"Header: {header2}")
+    for holding in holdings2:
         print(holding)
